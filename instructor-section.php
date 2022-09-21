bf15909cd41b458538d5ec4b615c878fbbc78ed3
jbellahou <56601030+jbellahou@users.noreply.github.com>
2022-09-21 13:52:23 -0500

    Update instructor-section.php

--- instructor-section.php
+++ instructor-section.php
@@ -34,7 +34,8 @@ if ($conn->connect_error) {
 $iid = $_GET['id'];
 echo $iid;
 $sql = "select section_id, section_number, i.instructor_name, c.prefix, c.number from section s join instructor i on i.instructor_id = s.instructor_id join course c on c.course_id = s.course_id where i.instuctor_id=" . $iid;
-$result = $conn->query($sql);
+echo $sql;
+    $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
