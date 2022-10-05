227d3c06b8ab74b63650d3d25965ef4e0113ac4b
jbellahou <56601030+jbellahou@users.noreply.github.com>
2022-10-05 14:17:50 -0500

    Update instructor-edit.php

--- instructor-edit.php
+++ instructor-edit.php
@@ -8,14 +8,46 @@
   </head>
   <body>
     <h1>Edit Instructor</h1>
+<?php
+$servername = "localhost";
+$username = "projecto_homework3";
+$password = "0w_zeP}]OVy0";
+$dbname = "projecto_homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT instructor_id, instructor_name from instructor where instructor_id=?";
+$stmt = $conn->prepare();
+$stmt->bind_param("i", $_POST['iid']);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
 <form method="post" action="instructor-edit-save.php">
   <div class="mb-3">
     <label for="instructorName" class="form-label">Name</label>
-    <input type="text" class="form-control" id="instructorName" aria-describedby="nameHelp" name="iName">
+    <input type="text" class="form-control" id="instructorName" aria-describedby="nameHelp" name="iName" value="<?=$row['instructor_name']?>">
     <div id="nameHelp" class="form-text">Enter the instructor's name.</div>
   </div>
+  <input type="hidden" name="iid" value="<?=$row['instructor_id']?>">
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
