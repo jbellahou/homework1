80f2977fc95e707a1c62b02bfeb2c2f8da5c35af
jbellahou <56601030+jbellahou@users.noreply.github.com>
2022-10-12 10:30:18 -0500

    Update instructors.php

--- instructors.php
+++ instructors.php
@@ -97,7 +97,7 @@ if ($result->num_rows > 0) {
       <form method="post" action="">
         <input type="hidden" name="iid" value="<?=$row["instructor_id"]?>" />
         <input type="hidden" name="saveType" value="Delete">
-        <button type="submit" class="btn" onclick="confirm('Are you sure?')">Delete</button>
+        <button type="submit" class="btn" onclick="return confirm('Are you sure?')">Delete</button>
       </form>
     </td>
   </tr>
