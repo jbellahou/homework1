e4dec6693b3c835f7a959787ae8160c424a979dc
jbellahou <56601030+jbellahou@users.noreply.github.com>
2022-10-12 09:02:36 -0500

    Update instructors.php

--- instructors.php
+++ instructors.php
@@ -66,7 +66,7 @@ $conn->close();
     <br />
     <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addInstructor">
-  Launch static backdrop modal
+  Add New
 </button>
 
 <!-- Modal -->
@@ -91,7 +91,6 @@ $conn->close();
   </div>
 </div>
     
-    <a href="instructor-add.php" class="btn btn-primary">Add New</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
